0a9c5220eaf677f00ef71926fab71dfc1f50b93d
Work <work@neils-macbook-air.local>
2018-11-15 12:47:32 -0500

    - Create initial single view project

---
+++ SingleSegueExample/SingleSegueExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleSegueExample
+//
+//  Created by Work on 11/15/18.
+//  Copyright © 2018 neilmrva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
